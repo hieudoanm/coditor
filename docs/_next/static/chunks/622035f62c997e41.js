6f1da6c7abd14abeab6e250e09b938bcb6863369
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-16 20:35:39 +0700

    update _app.tsx

--- docs/_next/static/chunks/622035f62c997e41.js
+++ docs/_next/static/chunks/622035f62c997e41.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/487c0c1aec6965fe.js",
+  "static/chunks/d9edb0e8400d9edf.js",
   "static/chunks/0f005616ed4aab83.js",
   "static/chunks/6053dfc69c72cc13.js",
-  "static/chunks/155fda952d96a3b3.css",
+  "static/chunks/fc71aad4d33e5d1a.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-165e09ba3ff9a2b5.js"
+  "static/chunks/turbopack-550cdedac9698db0.js"
 ])
